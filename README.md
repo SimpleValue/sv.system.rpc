a7d11d67b43c0297692365cfe02167e3189f7cba
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 14:33:00 +0200

    Extracted sv.system.rpc from sv/system.

---
+++ README.md
@@ -0,0 +1,12 @@
+# system.rpc
+
+Components and helper functions to use
+[sv.rpc.ring](https://github.com/SimpleValue/sv.rpc.ring) in a
+sv/system-based application.
+
+## License
+
+Copyright © 2016 Max Weber (SimpleValue)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sv/system.rpc "0.1.0-SNAPSHOT"
+  :description "Components and helper functions to use sv.rpc.ring in
+                a sv/system-based application."
+  :url "https://github.com/SimpleValue/sv.system.rpc"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [sv.rpc/ring "0.2.0"]])
